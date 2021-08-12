55d5e9f3dd877d5ee8e8cb5a67a3a5c3a3590390
s.guliaeva <s.guliaeva@corp.mail.ru>
2021-08-13 02:37:19 +0300

    control with keys

--- tabs/src/app/app.module.ts
+++ tabs/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {TabsModule} from './tabs/tabs.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {TabsModule} from './tabs/tabs.module';
     BrowserModule,
     AppRoutingModule,
     TabsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tabs/src/app/tabs/components/tab-header/tab-header.component.ts
+++ tabs/src/app/tabs/components/tab-header/tab-header.component.ts
@@ -22,4 +22,35 @@ export class TabHeaderComponent implements OnInit {
     }
   }
 
+  public handleKeyUp(event: KeyboardEvent | any) {
+    switch (event.key) {
+      case 'ArrowLeft':
+        if (event.target.previousSibling && !event.target.previousSibling.ariaDisabled) {
+          event.target.previousSibling.focus();
+        }
+        break;
+      case 'ArrowRight':
+        if (event.target.nextSibling && !event.target.nextSibling.ariaDisabled) {
+          event.target.nextSibling.focus();
+        }
+        break;
+      case 'Enter':
+        if (event.target) {
+          const id = +event.target.attributes.itemid.value;
+          this.selectTab(this.tabs.toArray()[id], id);
+        }
+    }
+  }
+
+  public handleKeyDown(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'Home':
+        event.preventDefault();
+        break;
+      case 'End':
+        event.preventDefault();
+        break;
+    }
+  }
+
 }
